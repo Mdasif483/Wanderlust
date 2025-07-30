4c436d54f93c48301bb6ec610dd970ab84ec515a
Mohd Asif <hiasif7786@gmail.com>
2025-07-30 11:35:40 +0530

    baseUrl Change

--- src/baseUrls.js
+++ src/baseUrls.js
@@ -1,2 +1,2 @@
- export const baseUrls = 'http://localhost:8000'
-// export const baseUrls = 'http://localhost:9000'
+//  export const baseUrls = 'http://localhost:8000'
+export const baseUrls = 'https://wanderlost-orej.onrender.com/'
